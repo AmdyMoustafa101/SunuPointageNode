6b3b0c39c9cbf8afc4d4e9e9c29b30f4b41cf47c
AmdyMoustafa101 <taphisllefa@gmail.com>
2025-01-09 18:40:30 +0000

    serveurs prets

--- server.js
+++ server.js
@@ -29,75 +29,72 @@ app.use('/api', historiqueRoutes);
 
 
 // Communication avec Arduino via SerialPort
-const loginPort = new SerialPort({
+const pointagePort = new SerialPort({
     path: '/dev/ttyUSB0', // Port pour attribution
     baudRate: 9600,
 });
 // const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 // const pointagePort = new SerialPort('/dev/ttyUSB0', { baudRate: 9600 });
-const loginParser = loginPort.pipe(new ReadlineParser({ delimiter: '\n' }));
+const pointageParser = pointagePort.pipe(new ReadlineParser({ delimiter: '\n' }));
 
-// pointageParser.on('data', async (data) => {
-//     const trimmedData = data.trim();
-//     if (!trimmedData.startsWith('UID:')) {
-//         console.log(`Donnée inattendue : ${trimmedData}`);
-//         return;
-//     }
+pointageParser.on('data', async (data) => {
+    const trimmedData = data.trim();
+    if (!trimmedData.startsWith('UID:')) {
+        console.log(`Donnée inattendue : ${trimmedData}`);
+        return;
+    }
 
-//     const rfidCardId = trimmedData.toUpperCase();
-//     console.log(`Carte RFID lue : ${rfidCardId}`);
-//     await handlePointage(rfidCardId);
-// });
+    const rfidCardId = trimmedData.toUpperCase();
+    console.log(`Carte RFID lue : ${rfidCardId}`);
+    await handlePointage(rfidCardId);
+});
 
-// // Fonction pour gérer le pointage
-// async function handlePointage(rfidCardId) {
-//     try {
-//         const response = await axios.get(`http://localhost:8002/api/get-user-by-card/${encodeURIComponent(rfidCardId)}`);
-//         const userData = response.data;
+// Fonction pour gérer le pointage
+async function handlePointage(rfidCardId) {
+    try {
+        const response = await axios.get(`http://localhost:8002/api/get-user-by-card/${encodeURIComponent(rfidCardId)}`);
+        const userData = response.data;
 
-//         if (!userData) {
-//             console.error('Utilisateur non trouvé');
-//             pointagePort.write('Utilisateur non trouvé\n');
-//             return;
-//         }
+        if (!userData) {
+            console.error('Utilisateur non trouvé');
+            pointagePort.write('Utilisateur non trouvé\n');
+            return;
+        }
 
-//         const date = new Date();
-//         const formattedDate = date.toISOString().split('T')[0];
-//         const currentTime = date.toTimeString().split(' ')[0].slice(0, 5);
+        const date = new Date();
+        const formattedDate = date.toISOString().split('T')[0];
+        const currentTime = date.toTimeString().split(' ')[0].slice(0, 5);
 
-//         let pointage = await Pointage.findOne({ userID: userData.id, date: formattedDate });
+        let pointage = await Pointage.findOne({ userID: userData.id, date: formattedDate });
 
-//         if (!pointage) {
-//             pointage = new Pointage({
-//                 userID: userData.id,
-//                 nom: userData.nom,
-//                 prenom: userData.prenom,
-//                 matricule: userData.matricule,
-//                 telephone: userData.telephone,
-//                 role: userData.role || 'employe',
-//                 date: formattedDate,
-//                 heure_arrivee: currentTime,
-//                 heure_depart: null,
-//                 vigile_nom: 'Pointage par carte',
-//                 vigile_matricule: 'Pointage par carte',
-//             });
-//             console.log('Pointage d\'arrivée enregistré.');
-//             pointagePort.write('Pointage Arrivee\n');
-//         } else if (!pointage.heure_depart) {
-//             pointage.heure_depart = currentTime;
-//             console.log('Pointage de départ enregistré.');
-//             pointagePort.write('Pointage Depart\n');
-//         } else {
-//             console.log('Pointage déjà complété pour la journée.');
-//             pointagePort.write('Pointage deja complete\n');
-//         }
+        if (!pointage) {
+            // Premier pointage de la journée
+            pointage = new Pointage({
+                userID: userData.id,
+                nom: userData.nom,
+                prenom: userData.prenom,
+                matricule: userData.matricule,
+                telephone: userData.telephone,
+                role: userData.role || 'apprenant',
+                date: formattedDate,
+                heure_arrivee: currentTime,
+                heure_depart: null,
+                vigile_nom: 'Pointage automatique',
+                vigile_matricule: 'Automatique',
+            });
+            pointagePort.write('Pointage Arrivee\n');
+        } else {
+            // Tous les autres scans mettent à jour l'heure de départ
+            pointage.heure_depart = currentTime;
+            pointagePort.write('Pointage Depart\n');
+        }
 
-//         await pointage.save();
-//     } catch (error) {
-//         console.error('Erreur lors du pointage :', error.response?.data?.message || error.message);
-//     }
-// }
+        await pointage.save();
+    } catch (error) {
+        console.error('Erreur lors du pointage :', error.response?.data?.message || error.message);
+    }
+}
 
 function notifyWebSocketClients(data) {
     wss.clients.forEach((client) => {
@@ -107,88 +104,96 @@ function notifyWebSocketClients(data) {
     });
 }
 
-let currentMode = null; // null, 'pointage', 'login'
+// Communication avec Arduino via SerialPort
+// const loginPort = new SerialPort({
+//     path: '/dev/ttyUSB0', // Port pour attribution
+//     baudRate: 9600,
+// });
 
-loginParser.on('data', async (data) => {
-    const trimmedData = data.trim();
-    if (!trimmedData.startsWith('UID:')) {
-        console.log(`Donnée inattendue : ${trimmedData}`);
-        return;
-    }
+// const loginParser = loginPort.pipe(new ReadlineParser({ delimiter: '\n' }));
 
-    const rfidCardId = trimmedData.toUpperCase();
-    console.log(`Carte RFID lue : ${rfidCardId}`);
+// let currentMode = null; // null, 'pointage', 'login'
 
-    // Exemple de logique pour choisir entre login et assignation
-    if (currentMode === 'login') {
-        notifyWebSocketClients({
-            type: 'card',
-            cardID: rfidCardId,
-          });
-        await handleLogin(rfidCardId);
-    } else {
-        await handleCardAssignment(rfidCardId);
-    }
-});
+// loginParser.on('data', async (data) => {
+//     const trimmedData = data.trim();
+//     if (!trimmedData.startsWith('UID:')) {
+//         console.log(`Donnée inattendue : ${trimmedData}`);
+//         return;
+//     }
 
-// Fonction pour gérer la connexion
-async function handleLogin(rfidCardId) {
-    try {
-        // Informer les clients WebSocket d'une carte scannée
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(rfidCardId); // Envoyer l'UID au client Angular
-            }
-        });
-        const response = await axios.post('http://localhost:8002/api/login-by-card', { cardID: rfidCardId });
-        if (response.data.message === 'Connexion réussie') {
-            console.log('Connexion réussie');
-            loginPort.write('Login success\n');
-        } else {
-            console.log('Connexion échouée');
-            loginPort.write('Login failed\n');
-        }
-    } catch (error) {
-        console.error('Erreur lors de la connexion :', error.response?.data?.message || error.message);
-        loginPort.write('Login failed\n');
-    }
-}
+//     const rfidCardId = trimmedData.toUpperCase();
+//     console.log(`Carte RFID lue : ${rfidCardId}`);
 
-// Fonction pour gérer l'attribution de carte
-async function handleCardAssignment(rfidCardId) {
-    try {
-        let selectedUser = null;
+//     // Exemple de logique pour choisir entre login et assignation
+//     if (currentMode === 'login') {
+//         notifyWebSocketClients({
+//             type: 'card',
+//             cardID: rfidCardId,
+//           });
+//         await handleLogin(rfidCardId);
+//     } else {
+//         await handleCardAssignment(rfidCardId);
+//     }
+// });
 
-        // Informer les clients WebSocket d'une carte scannée
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(rfidCardId); // Envoyer l'UID au client Angular
-            }
-        });
+// // Fonction pour gérer la connexion
+// async function handleLogin(rfidCardId) {
+//     try {
+//         // Informer les clients WebSocket d'une carte scannée
+//         wss.clients.forEach((client) => {
+//             if (client.readyState === WebSocket.OPEN) {
+//                 client.send(rfidCardId); // Envoyer l'UID au client Angular
+//             }
+//         });
+//         const response = await axios.post('http://localhost:8002/api/login-by-card', { cardID: rfidCardId });
+//         if (response.data.message === 'Connexion réussie') {
+//             console.log('Connexion réussie');
+//             loginPort.write('Login success\n');
+//         } else {
+//             console.log('Connexion échouée');
+//             loginPort.write('Login failed\n');
+//         }
+//     } catch (error) {
+//         console.error('Erreur lors de la connexion :', error.response?.data?.message || error.message);
+//         loginPort.write('Login failed\n');
+//     }
+// }
 
-        // Si aucun utilisateur n'est sélectionné, ne pas procéder à l'attribution
-        if (!selectedUser) {
-            console.log('Aucun utilisateur sélectionné pour cette carte.');
-            return;
-        }
+// // Fonction pour gérer l'attribution de carte
+// async function handleCardAssignment(rfidCardId) {
+//     try {
+//         let selectedUser = null;
 
-        // Payload pour l'API assign-card
-        const payload = {
-            uid: rfidCardId,
-            userType: selectedUser.userType, // ex: 'employes' ou 'apprenants'
-            userId: selectedUser.userId,
-        };
+//         // Informer les clients WebSocket d'une carte scannée
+//         wss.clients.forEach((client) => {
+//             if (client.readyState === WebSocket.OPEN) {
+//                 client.send(rfidCardId); // Envoyer l'UID au client Angular
+//             }
+//         });
 
-        // Appel API pour l'attribution
-        const response = await axios.post('http://localhost:8002/api/assign-card', payload);
-        console.log('Carte attribuée avec succès : ${response.data}');
-        console.log('Attribution de la carte...');
-        // Logique de votre attribution de carte ici
-        loginPort.write('Card assigned\n');
-    } catch (error) {
-        console.error('Erreur lors de l\'attribution de la carte :', error.response?.data?.message || error.message);
-    }
-}
+//         // Si aucun utilisateur n'est sélectionné, ne pas procéder à l'attribution
+//         if (!selectedUser) {
+//             console.log('Aucun utilisateur sélectionné pour cette carte.');
+//             return;
+//         }
+
+//         // Payload pour l'API assign-card
+//         const payload = {
+//             uid: rfidCardId,
+//             userType: selectedUser.userType, // ex: 'employes' ou 'apprenants'
+//             userId: selectedUser.userId,
+//         };
+
+//         // Appel API pour l'attribution
+//         const response = await axios.post('http://localhost:8002/api/assign-card', payload);
+//         console.log('Carte attribuée avec succès : ${response.data}');
+//         console.log('Attribution de la carte...');
+//         // Logique de votre attribution de carte ici
+//         loginPort.write('Card assigned\n');
+//     } catch (error) {
+//         console.error('Erreur lors de l\'attribution de la carte :', error.response?.data?.message || error.message);
+//     }
+// }
 
 // let isButtonPressed = false; // Indique si le bouton poussoir est pressé
 // let isLoginMode = false; // Nouveau mode pour la connexion
